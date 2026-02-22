ba2f782711bf01a6314cb5bffa8b105aab4881f8
Nona Takahara <nona_t_2612@yahoo.co.jp>
2026-02-22 20:46:57 +0900

    ESLintを更新

--- member-portal-frontend/eslint.config.mjs
+++ member-portal-frontend/eslint.config.mjs
@@ -1,17 +1,27 @@
 import { defineConfig, globalIgnores } from "eslint/config"
 import nextVitals from "eslint-config-next/core-web-vitals"
 import nextTs from "eslint-config-next/typescript"
+import prettier from "eslint-config-prettier"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+  {
+    rules: {
+      "no-console": "error",
+      eqeqeq: ["error", "always"],
+      camelcase: "warn"
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
-    "next-env.d.ts"
+    "next-env.d.ts",
+    "styled-system/**"
   ])
 ])
 
